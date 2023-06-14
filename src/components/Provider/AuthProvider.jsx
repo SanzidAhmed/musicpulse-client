79944f250edb10332d7d0c930222f165d03fda7b
Sanzid 2.0 <sanzid.official@gmail.comm>
2023-06-14 20:59:20 +0600

    implement jwt for secure

--- src/components/Provider/AuthProvider.jsx
+++ src/components/Provider/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { app } from "../../firebase/firebase.config";
+import axios from "axios";
 
 
 export const AuthContext = createContext(null)
@@ -35,6 +36,18 @@ const AuthProvider = ({children}) => {
     useEffect(() => {
         const unsubscribe =  onAuthStateChanged(auth, currentUser => {
               setUser(currentUser);
+
+            //   get and set token
+            if(currentUser){
+                axios.post('http://localhost:3500/jwt', {email: currentUser.email})
+                .then(data => {
+                    console.log(data.data.token);
+                    localStorage.setItem('access_token', data.data.token);
+                })
+            }
+            else{
+                localStorage.removeItem('access_token');
+            }
               setLoading(false)
           })
           return () => {
--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -3,10 +3,15 @@ import { useContext } from 'react';
 import { AuthContext } from '../components/Provider/AuthProvider';
 const useCart = () =>{
     const {user} = useContext(AuthContext);
+    const token = localStorage.getItem('access_token');
     const {  data: cart = [] , refetch } = useQuery({
         queryKey: ['carts', user?.email],
         queryFn: async () => {
-            const response = await fetch(`http://localhost:3500/carts?email=${user?.email}`)
+            const response = await fetch(`http://localhost:3500/carts?email=${user?.email}`, {
+                headers:{
+                    authorization: `bearer ${token}`
+                }
+            })
             return response.json();
         },
       })
